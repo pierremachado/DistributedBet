2851dff3897efccc3a179785726ac9b75d3a33a8
Valmir Filho <valmirnogueira514@gmail.com>
2024-12-13 15:08:33 -0300

    first impl event creating and gambling

--- contracts/BetContract.sol
+++ contracts/BetContract.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract BetContract {
+
+    uint256 constant MIN_PARTICIPANTS_PER_EVENT = 10;
+
     struct Bet {
         address gambler;
         uint8 bet;
         uint256 betValue;
+        uint256 currentOdd;
     }
 
     struct Event {
@@ -23,18 +27,16 @@ contract BetContract {
         uint256 amount;
     }
 
-    mapping(uint256 => Event) public allEvents;
+    Event[] allEvents;
+
     mapping(address => Account) public wallets;
 
-    event BetCreated(uint256 indexed id, string eventType, uint256 odd);
+    event EventCreated(uint256 indexed id, string eventType, uint256 odd);
     event BetClosed(uint256 indexed id, string eventType, uint8 result);
     event UserRegistered(address indexed client);
 
     modifier isRegistered(address client) {
-        require(
-            wallets[msg.sender].created,
-            "Cliente ainda nao cadastrado."
-        );
+        require(wallets[msg.sender].created, "Cliente ainda nao cadastrado.");
         _;
     }
 
@@ -65,7 +67,12 @@ contract BetContract {
         emit UserRegistered(msg.sender);
     }
 
-    function getWallet() public isRegistered(msg.sender) view returns (uint256) {
+    function getWallet()
+        public
+        view
+        isRegistered(msg.sender)
+        returns (uint256)
+    {
         return wallets[msg.sender].amount;
     }
 
@@ -75,7 +82,45 @@ contract BetContract {
     }
 
     function withdraw(uint256 value) public isRegistered(msg.sender) {
-        require(value <= wallets[msg.sender].amount, "Cliente nao ha saldo suficiente");
+        require(
+            value <= wallets[msg.sender].amount,
+            "Cliente nao ha saldo suficiente"
+        );
         payable(msg.sender).transfer(value);
+    }
+
+    function createEvent(string memory eventType, uint256 odd) public {
+        Event memory newEvent;
+        newEvent.creator = msg.sender;
+        newEvent.odd = odd;
+        newEvent.eventType = eventType;
+        allEvents.push(newEvent);
+        emit EventCreated(allEvents.length - 1, eventType, odd);
+    }
+
+    function bet(
+        uint256 eventId,
+        uint8 prediction,
+        uint256 betValue
+    )
+        public
+        creatorCanNotBet(eventId)
+        betIsActive(eventId)
+        isRegistered(msg.sender)
+    {
+        require(betValue > 0, "Valor nao permitido");
+        require(betValue <= wallets[msg.sender].amount, "Saldo insuficiente");
+
+        wallets[msg.sender].amount -= betValue;
+        allEvents[eventId].amount += betValue;
+
+        allEvents[eventId].participants.push(
+            Bet({
+                gambler: msg.sender,
+                bet: prediction,
+                betValue: betValue,
+                currentOdd: allEvents[eventId].odd
+            })
+        );
     }
 }
